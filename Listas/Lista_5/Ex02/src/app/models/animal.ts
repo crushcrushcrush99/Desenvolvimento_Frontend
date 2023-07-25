e04fc596e09486b07215203135a0c110c4412e7b
crushcrushcrush99 <rockletsamor@gmail.com>
2023-07-24 21:31:49 -0300

    modificando ex

--- Listas/Lista_5/Ex02/src/app/models/animal.ts
+++ Listas/Lista_5/Ex02/src/app/models/animal.ts
@@ -3,13 +3,16 @@ export class Animal{
   public nome: string;
   public raca: string;
   public peso: number;
+  public foto: string;
 
   constructor(nome: string,
               raca: string,
-              peso: number)
+              peso: number,
+              foto: string)
   {
     this.nome= nome;
     this.raca= raca;
     this.peso= peso;
+    this.foto= foto;
   }
 }
